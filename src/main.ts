7c506df53756819e0bd0d80cb2eeaee112f14819
Michal Miszczyszyn <michal@mmiszy.pl>
2020-06-02 00:26:27 +0200

    try uploading artefact instead

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
-import { getInput, setFailed } from "@actions/core";
+import { getInput, setFailed, info } from "@actions/core";
 import { context, GitHub } from "@actions/github";
+import { create, UploadOptions } from "@actions/artifact";
+import Fs from "fs";
+import Path from "path";
 // @ts-ignore
 import table from "markdown-table";
 import Term from "./Term";
@@ -15,10 +18,11 @@ async function run() {
       );
     }
 
-    const token = getInput("github_token");
+    // const token = getInput("github_token");
     const skipStep = getInput("skip_step");
     const buildScript = getInput("build_script");
-    const octokit = new GitHub(token);
+    // const octokit = new GitHub(token);
+    const name = getInput("artifact-name", { required: false });
     const term = new Term();
     const limit = new SizeLimit();
 
@@ -46,25 +50,41 @@ async function run() {
       throw error;
     }
 
-    const number = context.payload.pull_request.number;
-    const event = status > 0 ? "REQUEST_CHANGES" : "COMMENT";
+    // const number = context.payload.pull_request.number;
+    // const event = status > 0 ? "REQUEST_CHANGES" : "COMMENT";
     const body = [
       `## [size-limit](${SIZE_LIMIT_URL}) report`,
       table(limit.formatResults(base, current))
     ].join("\r\n");
 
     try {
-      octokit.pulls.createReview({
-        ...context.repo,
-        // eslint-disable-next-line camelcase
-        pull_number: number,
-        event,
-        body
-      });
-    } catch (error) {
-      console.log(
-        "Error creating PR review. This can happen for PR's originating from a fork without write permissions."
+      const artifactClient = create();
+      const options: UploadOptions = {
+        continueOnError: false
+      };
+      const path = Path.join(__dirname, "size-limit.md");
+      Fs.writeFileSync(path, body, "utf8");
+
+      const uploadResponse = await artifactClient.uploadArtifact(
+        name || "artifact",
+        [path],
+        __dirname,
+        options
       );
+
+      if (uploadResponse.failedItems.length > 0) {
+        setFailed(
+          `An error was encountered when uploading ${uploadResponse.artifactName}.`
+        );
+      } else if (status > 0) {
+        setFailed(`Failed.`);
+      } else {
+        info(
+          `Artifact ${uploadResponse.artifactName} has been successfully uploaded!`
+        );
+      }
+    } catch (error) {
+      console.log("Error creating PR review.");
     }
   } catch (error) {
     setFailed(error.message);
